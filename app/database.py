87591da378f8707a5b49e617684a6763f2225997
rakshith <rakshith1@gmail.com>
2022-06-07 16:13:32 +0530

    commit

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from psycopg2.extras import RealDictCursor 
 import time
 from .config import settings
 
