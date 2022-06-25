a5c26e68242258aa5aef9cf9bde5171fa2a8e96c
rakshith <rakshith1@gmail.com>
2022-06-25 09:55:25 +0530

    bank calculations test

--- app/calculations.py
+++ app/calculations.py
@@ -9,3 +9,16 @@ def multiply(num1: int, num2: int):
 
 def divide(num1: int, num2: int):
     return num1/num2
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit(self,amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def collect_intrest(self):
+        self.balance *= 1.1
--- tests/test_calcuations.py
+++ tests/test_calcuations.py
@@ -1,4 +1,4 @@
-from app.calculations import add, subract, multiply, divide
+from app.calculations import add, subract, multiply, divide,BankAccount
 import pytest
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -18,3 +18,21 @@ def test_multiply():
 def test_divide():
     assert divide(9,3)==3
 
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+def test_withdraw():
+    bank_account =BankAccount(50)
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+def test_deposit():
+    bank_account =BankAccount(50)
+    bank_account.deposit(100)
+    assert bank_account.balance == 150
+
+def test_intrest():
+    bank_account =BankAccount(100)
+    bank_account.collect_intrest()
+    assert bank_account.balance == 110.00000000000001
