a81f17e54c03a464ade7c6e5f0586c38139b7fe6
rakshith <rakshith1@gmail.com>
2022-07-01 13:15:11 +0530

    pipeline

--- app/models.py
+++ app/models.py
@@ -32,3 +32,5 @@ class Vote(Base):
     __tablename__="votes"
     user_id=Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
     post_id=Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+
+    
